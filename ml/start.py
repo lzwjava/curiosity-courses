2f3e59928449b77ca3f76f9905573287e5dfd0e7
lzwjava <lzwjava@gmail.com>
2021-03-13 14:05:13 +0800

    a

--- ml/start.py
+++ ml/start.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 x = np.random.rand(2)*100
 y = np.random.rand(2)*100
 
-a_max=1000
-a_min=-1000
-b_max=1000
-b_min=-1000
+a_max=100
+a_min=-100
+b_max=100
+b_min=-100
 
 def cal_d(da, b):
     y0 = x[0] * da + b
@@ -28,26 +28,24 @@ def avg_a():
 def avg_b():
     return (b_max + b_min) / 2
 
-for i in range(100):
-    a = avg_a()
-    b = avg_b()
-    max_d = cal_d(a_max, b)
-    min_d = cal_d(a_min, b)
-    if max_d < min_d:
-        a_min = a
-    else:
-        a_max = a
-    # a = avg_a()
-    # max_db = cal_db(a, b_max)
-    # min_db = cal_db(a, b_min)
-    # if max_db < min_db:
-    #     b_min = b        
-    # else:
-    #     b_max = b
+min_dist = 1000000
+min_a = 0
+min_b = 0
+
+for i in range(10000):
+    a = np.random.randint((a_max - a_min)*10)/10 + a_min
+    b = np.random.randint((b_max - b_min)*10)/10 + b_min
+    y0 = a * x[0] + b
+    y1 = a * x[1] + b
+    dist = abs(y0 - y[0]) + abs(y1- y[1])
+    if (dist < min_dist):
+        min_a = a
+        min_b = b
+        min_dist = dist
 
 print(x)
 print(y)
-print('a = ', avg_a())
-print('b = ', avg_b())
-print('y[0]=', avg_a() * x[0] + avg_b())
-print('y[1]=', avg_a() * x[1] + avg_b())
+print('a = ', min_a)
+print('b = ', min_b)
+print('y[0]=', min_a * x[0] + min_b)
+print('y[1]=', min_a * x[1] + min_b)
