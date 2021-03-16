1a7e0601d5cf3f3267e90a76d32b1bde1c67d6cd
lzwjava <lzwjava@gmail.com>
2021-03-16 14:10:04 +0800

    a

--- feynman/feynman.md
+++ feynman/feynman.md
@@ -312,3 +312,58 @@ l.12 \tfrac{1}{2}
 \[\tfrac{1}{2}mv^2\]
 ```
 
+进行各种试探。
+
+```python
+from bs4 import BeautifulSoup
+from latex2svg import latex2svg
+
+file = open('The Feynman Lectures on Physics Vol. I Ch. 13_ Work and Potential Energy (A).html')
+content = file.read()
+
+soup = BeautifulSoup(content, features="lxml")
+
+mathjaxs = soup.findAll('script', {'type': 'math/tex'})
+for mathjax in mathjaxs:
+    print(mathjax.string)
+    wrap = '$' + mathjax.string + '$'
+    # if 'frac' in mathjax.string:
+    #     wrap = '$' + mathjax.string + '$'
+    if 'FLP' in mathjax.string:
+        continue
+    elif 'Fig' in mathjax.string:
+        continue
+    elif 'eps' in mathjax.string:
+        continue
+    out = latex2svg(wrap)
+    # print(out)
+    node = BeautifulSoup(out['svg'], features="lxml")
+    svg = node.find('svg')
+    mathjax.insert_after(svg)
+    # print(out['svg'])
+    # break
+    # mathjax.replaceWith(out['svg'])    
+    
+    # print(dir(mathjax))
+    # break
+    
+    # out = latex2svg(wrap)    
+    # print(out['svg'])
+
+# print(len(soup.contents))
+    
+output_file = open('out.html', 'w')
+output_file.write(soup.prettify())
+output_file.close()
+# print(soup.contents)
+
+# out = latex2svg(r'\( e^{i \pi} + 1 = 0 \)')
+# print(out['depth'])
+# print(out['svg'])
+
+# svg = open('1.svg', 'w')
+# svg.write(out['svg'])
+# svg.close()
+
+```
+
--- feynman/feynman.py
+++ feynman/feynman.py
@@ -1,16 +1,59 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup,Tag
+from bs4.element import NavigableString
 from latex2svg import latex2svg
 
 file = open('The Feynman Lectures on Physics Vol. I Ch. 13_ Work and Potential Energy (A).html')
 content = file.read()
 
-soup = BeautifulSoup(content)
+soup = BeautifulSoup(content, features="lxml")
+
+def decompose_all(node):
+    node.decompose()
+previews = soup.findAll('span', {'class': 'MathJax'})
+for preview in previews:
+    preview.decompose()
+
+previews = soup.findAll('div', {'class': 'MathJax_Display'})
+for preview in previews:
+    preview.decompose()
+
+previews = soup.findAll('span', {'class': 'MathJax_Preview'})
+for preview in previews:
+    preview.decompose()
 
 mathjaxs = soup.findAll('script', {'type': 'math/tex'})
 for mathjax in mathjaxs:
     print(mathjax.string)
-    out = latex2svg(mathjax.string)
-    print(out['svg'])
+    wrap = '$' + mathjax.string + '$'
+    # if 'frac' in mathjax.string:
+    #     wrap = '$' + mathjax.string + '$'
+    if 'FLP' in mathjax.string:
+        continue
+    elif 'Fig' in mathjax.string:
+        continue
+    elif 'eps' in mathjax.string:
+        continue
+    out = latex2svg(wrap)
+    # print(out)
+    node = BeautifulSoup(out['svg'], features="lxml")
+    svg = node.find('svg')
+    mathjax.insert_after(svg)
+    # print(out['svg'])
+    # break
+    # mathjax.replaceWith(out['svg'])    
+    
+    # print(dir(mathjax))
+    # break
+    
+    # out = latex2svg(wrap)    
+    # print(out['svg'])
+
+# print(len(soup.contents))
+    
+output_file = open('out.html', 'w')
+output_file.write(soup.prettify())
+output_file.close()
+# print(soup.contents)
 
 # out = latex2svg(r'\( e^{i \pi} + 1 = 0 \)')
 # print(out['depth'])
